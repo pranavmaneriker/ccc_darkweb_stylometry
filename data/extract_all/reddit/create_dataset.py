b34ada587b1e0164ef61cfbc5ffdfd546628f955
Pranav Maneriker <pranavmane@gmail.com>
2023-06-20 04:13:16 -0400

    Not sorted

--- data/extract_all/reddit/create_dataset.py
+++ data/extract_all/reddit/create_dataset.py
@@ -1,8 +1,9 @@
 # %%
 import os
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 import json
 import duckdb
+from typing import List
 
 # %%
 data_dir = "/data/reddit/torrents/data/reddit/comments/extracted/"
@@ -22,14 +23,27 @@ max_count = 2000
 tablename = fname.replace("-", "_")
 table = duckdb.read_json(os.path.join(data_dir, fname))
 conn = duckdb.register(tablename, table)
+conn.execute("PRAGMA enable_progress_bar=true")
 
 
 # %%
+def is_sorted(l: List[int]) -> bool:
+    return all([l[i] <= l[i+1] for i in range(len(l) - 1)])
+
+duckdb.create_function("is_sorted", is_sorted)
+print("Creating Reddit 2018")
+print("Checking sorted")
+
+check_sorted_all = duckdb.sql(f"SELECT DISTINCT(sorted) FROM \
+                                 (SELECT is_sorted(LIST(created_utc)) AS sorted, {author_col} \
+                                 FROM {tablename} GROUP BY {author_col}) t")
+print("ALl sorted?", check_sorted_all)
+print("Creating table")
 sel_cols = ",".join([f"list({col}) as {col}" for col in non_auth_columns])
 all_rows = duckdb.sql(f"COPY (SELECT {sel_cols}, {author_col} FROM {tablename} GROUP BY {author_col}\
                          HAVING len(list(id)) <= {max_count} \
-                      ) TO '/data/ccc/data/reddit_{tablename}' (FORMAT JSON)"
-                      ,connection=conn)
+                      ) TO '/data/ccc/data/reddit_{tablename}' (FORMAT JSON)", 
+                      connection=conn)
 
 # %% [markdown]
 # ***
@@ -47,13 +61,16 @@ def jsonl_gen(fname = "RC_2018-01",
     table = duckdb.read_parquet(os.path.join(data_dir, fname))
     conn = duckdb.register(tablename, table)
     conn.execute(f"PRAGMA memory_limit='{max_memory}'")
+    conn.execute("PRAGMA enable_progress_bar=true")
     sel_cols = ",".join([f"list({col}) as {col}" for col in non_auth_columns])
     all_rows = duckdb.sql(f"COPY (SELECT {sel_cols}, {author_col} FROM {tablename} GROUP BY {author_col} \
-                            HAVING len(list(id)) <= 2000) \
+                            HAVING len(list(id)) <= {max_count} \
+                            ORDER BY created_utc) \
                            TO '/data/ccc/data/reddit_{tablename}' (FORMAT JSON)"
                           , connection=conn)
 
 # %%
+print("Creating Reddit 2019")
 jsonl_gen(fname="RC_2019-12.parquet", tablename="RC_2019_12")
 
 # %%
